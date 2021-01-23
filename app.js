e885fc6b271dbeb4318aeaa39abeef7107706f6b
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-01-24 01:28:17 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(3000, () => {console.log("Server started");});
